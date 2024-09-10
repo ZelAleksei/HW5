3c76c1de33bda544e73531291a49ad427238fa60
Aleksey Zelianin <azelyanin@internet.ru>
2024-09-10 14:14:32 +0300

    Добавление javadoc

--- src/Run.java
+++ src/Run.java
@@ -1,6 +1,12 @@
 
 public class Run {
 
+    /**
+     * Метод запуска программы
+     * @param bankAccountOwnerName - имя аккаунта, в формате String
+     * @param bankAccountBalance - баланс счёта
+     * @throws ExclusionOfInsufficientFunds - исключение недостатка средств на балансе
+     */
     public void run(String bankAccountOwnerName, float bankAccountBalance) throws ExclusionOfInsufficientFunds {
         System.out.println("Добро пожаловать!");
         Entering ent = new Entering();
@@ -12,9 +18,16 @@ public class Run {
         }
     }
 
+    /***
+     * Функция проверки имени аккаунта
+     * @param name - имя введённое пользователем
+     * @param bankAccountOwnerName - имя указанное в main
+     * @return - возвразает булево значение результата проверки
+     * @throws ExclusionNameInconsistency - исключение при не совпадении имён
+     */
     public static boolean checkName(String name, String bankAccountOwnerName)
             throws ExclusionNameInconsistency {
-        boolean check = false;
+        boolean check;
         if (!name.equals(bankAccountOwnerName)) {
             throw new ExclusionNameInconsistency("Имя клиента не соответствует!");
         } else {
@@ -24,6 +37,12 @@ public class Run {
         return check;
     }
 
+    /***
+     * Метод проверки возможности снятия суммы
+     * @param sum - введёная пользовотелем сумма для снятия
+     * @param bankAccountBalance - баланс счёта, указанного в main
+     * @throws ExclusionOfInsufficientFunds - исключение при снятии больщей суммы чем есть на балансе
+     */
     public static void checkSum(float sum, float bankAccountBalance) throws ExclusionOfInsufficientFunds {
         if (sum <= bankAccountBalance) {
             System.out.println("Операция выполнена успешно. \n Ваш баланс: " + (bankAccountBalance - sum));
