3527a9790a14612b6748cffb27a31e4ed937399f
Aleksey Zelianin <azelyanin@internet.ru>
2024-09-10 14:52:32 +0300

    Редактирование класса Entering. добавление проверки корректности ввода суммы

--- src/Entering.java
+++ src/Entering.java
@@ -1,7 +1,12 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Entering {
 
+    /**
+     * Функция ввода имени
+     * @return возвращает введёное пользователем имя
+     */
     public String enterNameClient() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите Ваше имя: ");
@@ -9,10 +14,25 @@ public class Entering {
         return nameClient;
     }
 
-    public float enteringSum() {
+
+    /**
+     * Функция ввода суммы для снятия с баланса, вслучае ввода букв пробрасывается исключение
+     * InputMismatchException и предлагается повторный ввод
+     * @return возвращает сумму для снятия с баланса в формате float
+     * @throws InputMismatchException - исключение ввода букв вместо цифр
+     */
+    public float enteringSum() throws InputMismatchException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите необходимую сумму: ");
-        float sum = sc.nextFloat();
+        float sum = 0;
+        try {
+            sum = sc.nextFloat();
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка ввода суммы для снятия. \n Повторите ввод.");
+            enteringSum();
+        }
+        sc.close();
         return sum;
     }
 }
+
